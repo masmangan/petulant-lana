ed188566ff77feda55c6af946141469eb41a753e
Marco Mangan (@FACIN 312) <marco.mangan@gmail.com>
2015-05-29 20:17:12 -0300

    app para teste; addEdge e addVertice, toString

--- src/pucrs/alpro3/graph/UndirectedGraphMatrix.java
+++ src/pucrs/alpro3/graph/UndirectedGraphMatrix.java
@@ -1,6 +1,7 @@
 package pucrs.alpro3.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UndirectedGraphMatrix implements UndirectedGraph {
@@ -9,41 +10,54 @@ public class UndirectedGraphMatrix implements UndirectedGraph {
 	private List<String> names;
 
 	public UndirectedGraphMatrix() {
-		// TODO inicializar matrix e lista names
+		matrix = new boolean[5][5];
+		names = new ArrayList<String>();
 	}
 
 	@Override
 	public void addVertice(String vertice) {
-		// TODO acrescentar o nome na lista names, caso o nome não exista
+		if (vertice == null)
+			throw new IllegalArgumentException("O vertice nao pode ser null");
+
+		if (vertice.trim().isEmpty())
+			throw new IllegalArgumentException("O vertice nao pode ser vazio");
+			
+		if (names.contains(vertice))
+			throw new IllegalArgumentException("O vertice ja se encontra cadastrado: " + vertice);
+		
+		names.add(vertice);
 	}
 
 	@Override
 	public void addEdge(String strOrig, String strDest) {
-		// TODO consultar a posição do nome de strOrig
-		// TODO consultar a posição do nome de strDest
-		// TODO na matriz, marcar como true a coordenada [pos(strOrig)][pos(strDest)]
+		int posOrig = names.indexOf(strOrig);
+		int posDest = names.indexOf(strDest);
+		matrix[posOrig][posDest] = true;
 	}
 
 	@Override
 	public int getDegree(String vertice) {
-		// TODO obter adjacentes do vertice
-		// TODO retornar o número de adjacentes
-		return 0;
+		return getAllAdjacents(vertice).size();
 	}
 
 	@Override
 	public ArrayList<String> getAllAdjacents(String vertice) {
+		ArrayList<String> r = new ArrayList<>();
 		// TODO consultar posicao do vertice
 		// TODO percorrer a linha da posição do vertice
 		// TODO colocar o nome de cada vertice na lista
 		// TODO retornar a lista
-		return null;
+		return r;
 	}
 
 	@Override
 	public String toString() {
-		// TODO apresentar matriz e lista
-		return super.toString();
+		String r = "";
+		r += names.toString();
+		for (int i = 0; i < matrix.length; i++) {
+			r += "\n" + Arrays.toString(matrix[i]);
+		}
+		return r;
 	}
 
 }
---
+++ src/pucrs/alpro3/graph/UndirectedGraphMatrixApp.java
@@ -0,0 +1,18 @@
+package pucrs.alpro3.graph;
+
+public class UndirectedGraphMatrixApp {
+public static void main(String[] args) {
+	UndirectedGraph g = new UndirectedGraphMatrix();
+	System.out.println(g);
+	g.addVertice("POA");
+	System.out.println(g);
+	g.addVertice("SDU");
+	System.out.println(g);
+	g.addVertice("CWB");
+	System.out.println(g);
+	g.addEdge("POA", "CWB");
+	System.out.println(g);
+	g.addEdge("POA", "SDU");
+	System.out.println(g);
+}
+}
